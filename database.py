bc946391cd6e58af52b1656a270748720ca771ac
Diegooooo19483 <damelo35@ucatolica.edu.co>
2025-11-30 18:13:20 -0500

    cloud

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = os.environ.get("postgresql://upiybnwgfiqs9yy4oxtf:JaBiK1EK5aLZBXVETk33ORHQkO0ctx@bjkgitbhafdgcvibtsar-postgresql.services.clever-cloud.com:50013/bjkgitbhafdgcvibtsar")
+# Clever Cloud te da la URL completa en la variable de entorno
+DATABASE_URL = os.getenv("postgresql://ubtfbwjh42jad5ld8zuk:uWkxWmhwyHPgw5xRgdktHNbflQJGAT@bckvhebytlohu0owjmv9-postgresql.services.clever-cloud.com:50013/bckvhebytlohu0owjmv9")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
