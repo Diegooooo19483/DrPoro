d7a3ebb21c4a64ee355c05e265f5e39bb3415882
Diegooooo19483 <damelo35@ucatolica.edu.co>
2025-11-30 16:46:08 -0500

    vss

--- database.py
+++ database.py
@@ -1,12 +1,9 @@
-# database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./dr_poro.db"
+DATABASE_URL = os.environ.get("postgresql://upiybnwgfiqs9yy4oxtf:JaBiK1EK5aLZBXVETk33ORHQkO0ctx@bjkgitbhafdgcvibtsar-postgresql.services.clever-cloud.com:50013/bjkgitbhafdgcvibtsar")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
